bfd76ffe99faa67286b68bd361725e08475a1952
KaranDF <89782521+KaranDF@users.noreply.github.com>
2021-10-08 16:18:26 +0530

    Add files via upload

--- datamodeling.java
+++ datamodeling.java
@@ -1,25 +1,33 @@
 
 import java.util.*;
+
+
+
 import java.math.BigDecimal;
-import java.util.Comparator;
-
-
 
 class Account {
 
-
-    
+    List<Account> Acc = new ArrayList<Account>();
+    int AccountId;
+    static int count = 0;
     public String Accname;
     public Long phone;
     public BigDecimal revenue;
-    public String address;
-
-    public Account(String Accname, Long phone, BigDecimal revenue, String address) {
+    public List<Contact> relatedContacts;
+    public Integer id;
+    public Account(String Accname, Long phone, BigDecimal revenue) {
         this.Accname = Accname;
         this.phone = phone;
         this.revenue = revenue;
-        this.address = address;
     }
+    Account(String Accname){
+        this.Accname = Accname;
+        AccountId = (++count);
+        Acc.add(this);
+    }
+    
+   
+
 
     public String getName() {
         return Accname;
@@ -30,6 +38,9 @@ class Account {
     }
     public BigDecimal getrevenue() {
         return revenue;
+    }
+    public Integer GetId(){
+        return id;
     }
 }
 class Contact {
@@ -42,11 +53,15 @@ class Contact {
     public Contact(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        
     }
 
     public String getName() {
         return firstName;
     }
+
+
+    
 }
 
 class Opportunity {
@@ -89,14 +104,14 @@ public class datamodeling {
     private static final Account[] Acc = null;
 
     public static void main(String[] args) {
-
+        //Accounts
         List<Account> Acc = new ArrayList<Account>();
         System.out.println("\n Accounts \n");
-        Account a1 = new Account("Google", (long) 834454583, (new BigDecimal("1000")), "Delhi");
-        Account a2 = new Account("DemandFarm", (long) 865555583, (new BigDecimal("20000")), "Pune");
-        Account a3 = new Account("Persistent", (long) 823336683, (new BigDecimal("50000")), "Nagpur");
-        Account a4 = new Account("Hundai", (long) 823336683, (new BigDecimal("7000")), "Gujrat");
-        Account a5 = new Account("Honda City", (long) 823336683, (new BigDecimal("90000")), "UP");
+        Account a1 = new Account("Google", (long) 834454583, (new BigDecimal("1000")));
+        Account a2 = new Account("DemandFarm", (long) 865555583, (new BigDecimal("20000")));
+        Account a3 = new Account("Persistent", (long) 823336683, (new BigDecimal("50000")));
+        Account a4 = new Account("Hundai", (long) 823336683, (new BigDecimal("7000")) );
+        Account a5 = new Account("Honda City", (long) 823336683, (new BigDecimal("90000")) );
 
         //Adding Accounts
         Acc.add(a1);
@@ -105,9 +120,11 @@ public class datamodeling {
         Acc.add(a4);
         Acc.add(a5);
         
-        
+        for(Account a: Acc){
+            System.out.println(a.Accname+" "+ a.revenue+ " "+ a.phone);
+        }
         //List of all accounts Where revenue > 10000
-        System.out.println("List of all accounts Where revenue > 10000 \n" );
+        System.out.println("\n List of all accounts Where revenue > 10000 \n" );
         for (Account a : Acc) 
         {
             
@@ -129,32 +146,48 @@ public class datamodeling {
                return c1.compareTo(c2);
                
            }});
-
            for (Account a : Acc) {
                System.out.println(a.Accname+" "+ a.revenue);
            }
 
-         
-       
-        // System.out.println("Sorting account by revenue:");  
-        // for(Account a : Acc){
-        //     System.out.println(a);
-        // }
-       
-
+           
         
+         //Contacts 
+ 
         List<Contact> Con = new ArrayList<Contact>();
+      Contact reportsTo;
         System.out.println("\n Contacts \n");
         Contact con1 = new Contact("Nikunj", "Sharma");
         Contact con2 = new Contact("Ashish", "Agrawal");
-
+        Contact con3 = new Contact("MS", "Dhoni");
+        Contact con4 = new Contact("Virat", "Agrawal");
+        Contact con5 = new Contact("Raina", "shetty");
+          
         Con.add(con1);
         Con.add(con2);
+        Con.add(con3);
+        Con.add(con4);
+        Con.add(con5);
 
         for (Contact con : Con) {
-            System.out.println(con.firstName);
+            System.out.println(con.firstName + " "+ con.lastName);
         }
+        
+        Map<Integer, List<Contact>> AccountRtoconMap = new HashMap<>();
 
+        for(Account a : Acc){
+            AccountRtoconMap.put(a.AccountId,a.relatedContacts);
+        }
+        // System.out.println("Account ID:List<Contacts> ");
+        // System.out.println(AccountRtoconMap);
+        // con3.reportsTo(con1);
+        // con4.reportsTo(con2);
+        // con5.reportsTo(con3);
+        // con2.reportsTo(con3);
+        // System.out.print(con1.reportsTo(con3));
+
+
+        //Opportunity
         List<Opportunity> Opp = new ArrayList<Opportunity>();
         enum Stages{NeedsAnalysis, ClosedWon, Qualification, ClosedLost, Prospecting}
         System.out.println("\n Opportunities \n");
@@ -169,16 +202,17 @@ public class datamodeling {
         Opp.add(op3);
         Opp.add(op4);
         Opp.add(op5);
-        // for(Opportunity op: Opp){
-        //     System.out.println(op.oppName+ " "+ op.revenue + " " + op.getStages());
-        // }
-        System.out.println("Printing the opportunity where stages is equal to Prospecting : ");
+
+        for(Opportunity op: Opp){
+            System.out.println(op.oppName+ " "+ op.amount + " " + op.getStages());
+        }
+
+        System.out.println("\n Printing the opportunity where stages is equal to Prospecting :- \n ");
         for (Opportunity op : Opp) {
             if (op.getStages() == Stages.Prospecting)
           
                 System.out.println(op.oppName + " " + "Stage: " + op.getStages());
         }
     }
+    
 }
-
-//Contacts hirarchy
